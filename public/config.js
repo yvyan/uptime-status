9d6be1cd93d76edba00db0b6d5f6dd5aa618a706
wing <843862803@qq.com>
2023-05-10 21:21:22 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -6,14 +6,11 @@ window.Config = {
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
-    'm784488775-dd1ad84b209c05f8e185c33e',
-    'm784490063-7b5da437e7f1e0d67613714d',
-    'm784497419-de55aa09902ccb3ab22d548a',
-    'm784496436-71a4bf7b1e3bdf7756be131b',
+    'ur2126217-dd9829e80445531b3e261391'
   ],
 
   // 日志天数
-  CountDays: 90,
+  CountDays: 60,
 
   // 是否显示检测站点的链接
   ShowLink: true,
@@ -21,16 +18,16 @@ window.Config = {
   // 导航栏菜单
   Navi: [
     {
-      text: 'Homepage',
-      url: 'https://status.org.cn/'
+      text: '导航页',
+      url: 'https://my.yvyan.top/'
+    },
+    {
+      text: 'uptimerobot.com',
+      url: 'https://uptimerobot.com/'
     },
     {
       text: 'GitHub',
       url: 'https://github.com/yb/uptime-status'
-    },
-    {
-      text: 'Blog',
-      url: 'https://abo.xyz/'
     },
   ],
 };
